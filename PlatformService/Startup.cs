f9991dc4b5a985e1b500754e745d34ce47a450c7
Buk7op <robxviktor@gmail.com>
2021-09-14 16:40:47 +0300

    DebugChanges

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -39,7 +39,6 @@ namespace PlatformService
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
             });
             Console.WriteLine($"--> CommandService Endpoint {Configuration["CommandService"]}");
-            Console.WriteLine($"--> CommandService Endpoint {Configuration["CommandService"]}");
         }
 
         
