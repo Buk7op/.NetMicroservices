66581d492e20b66f0396cd5c910a8196796d4a98
Виктор Петухов <victor_82hp@mail.ru>
2021-10-18 11:48:16 +0300

    added a messageBus client

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using PlatformService.AsyncDataServices;
 using PlatformService.Data;
 using PlatformService.SyncDataServices.Http;
 
@@ -45,6 +46,7 @@ namespace PlatformService
             
             services.AddScoped<IPlatformRepo,PlatformRepo>();
             services.AddHttpClient<ICommandDataClient,HttpCommandDataClient>();
+            services.AddSingleton<IMessageBusClient,MessageBusClient>();
             services.AddControllers();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSwaggerGen(c =>
