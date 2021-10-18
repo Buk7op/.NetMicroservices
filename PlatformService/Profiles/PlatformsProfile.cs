da7d5aacd7fe585e24e0c74a43b232a57e0f3510
Виктор Петухов <victor_82hp@mail.ru>
2021-10-18 11:21:42 +0300

    Added a new dto Published for publish info into message bus, also added mapper readDto-> publishedDto

--- PlatformService/Profiles/PlatformsProfile.cs
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -11,6 +11,7 @@ namespace PlatformService.Profiles
             // Source -> target
             CreateMap<Platform, PlatformReadDto>();
             CreateMap<PlatformCreateDto, Platform>();
+            CreateMap<PlatformReadDto,PlatformPublishedDto>();
         }
     }
 }
